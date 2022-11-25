aeca47fbbf1ce9a950431d33cad336f8a3a5bb06
Adby Santos <adby.santos15@outlook.com>
2022-11-24 23:37:08 -0300

    feat: minor updates

--- nativeapp/app/src/main/java/com/example/nativeapp/MainActivity.kt
+++ nativeapp/app/src/main/java/com/example/nativeapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +21,7 @@ class MainActivity : AppCompatActivity() {
                 .build(this@MainActivity)
         );
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@MainActivity,
                 HelloWorldActivity::class.java)
 
